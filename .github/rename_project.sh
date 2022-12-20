a82d8b5e64263d2e58203c5f7e060167692a172c
jthigpen <jthigpen@users.noreply.github.com>
2022-12-20 18:03:13 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="clora"
+original_name="shortcutclient"
+original_urlname="shortcutclient"
+original_description="Awesome shortcutclient created by clora"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv shortcutclient $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
